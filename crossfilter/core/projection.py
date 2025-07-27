07d910993be3f39113b86e6160dc842e26357ffd
Thad Hughes <thad@imbue.com>
2025-07-27 12:18:31 -0700

    Working on new projections.

--- crossfilter/core/projection.py
+++ crossfilter/core/projection.py
@@ -1,11 +1,93 @@
 from dataclasses import dataclass
+from typing import Any, Final
+import uuid
+from crossfilter.core.data_subset import DataSubset
 
 import pandas as pd
 
 
 @dataclass
+class _Group:
+    group_key: tuple[Any, ...]
+    group_df: pd.DataFrame
+    unbucketed_row_count: int = -1
+    optional_bucketed_on_column: str | None = None
+
+    def __post_init__(self) -> None:
+        self.unbucketed_row_count = len(self.group_df)
+
+
+def _maybe_bucket_group(
+    group: _Group,
+    max_entries: int,
+    bucketing_candidate_columns: tuple[str, ...],
+) -> None:
+    if len(group.group_df) <= max_entries:
+        return
+
+    raise NotImplementedError("Not implemented")
+    # for bucketing_candidate_column in bucketing_candidate_columns:
+    #     if group.group_df[bucketing_candidate_column].nunique() <= max_entries:
+    #         group.bucketed_on_column = bucketing_candidate_column
+    #         bucketed_df = group.group_df.groupby(
+    #             bucketing_candidate_column, dropna=False
+    #         )
+
+
+@dataclass
 class GroupedBucketedProjection:
     projection_uuid: str
-    grouped_by_columns: list[str]
 
+    # Each group may be bucketed by a different column, depending on how many rows are in the group.
+    grouped_by_columns: tuple[str, ...]
+
+    # Key: how is this indexed?  Can it be (group_key)->bucket_key|uuid?
     projected_data: pd.DataFrame
+
+
+def create_grouped_bucketed_projection(
+    ds: DataSubset,
+    group_by_columns: tuple[str, ...],
+    # Determines whether we need to bucket.
+    max_entries_per_group: int,
+    bucketing_candidate_columns: tuple[str, ...],
+    sort_groups_by_cardinality: bool,
+) -> GroupedBucketedProjection:
+
+    groups: list[_Group] = []
+    if group_by_columns:
+        for group_key, group_df in ds.df.groupby(group_by_columns, dropna=False):
+            group = _Group(group_key=group_key, group_df=group_df)
+            groups.append(group)
+    else:
+        group = _Group(group_key=(), group_df=ds.df)
+        groups.append(group)
+
+    for group in groups:
+        _maybe_bucket_group(group, max_entries_per_group, bucketing_candidate_columns)
+
+    if sort_groups_by_cardinality:
+        groups.sort(key=lambda x: (-x.unbucketed_row_count, x.group_key))
+    else:
+        groups.sort(key=lambda x: x.group_key)
+
+    return GroupedBucketedProjection(
+        projection_uuid=str(uuid.uuid4()),
+        grouped_by_columns=group_by_columns,
+        projected_data=pd.concat(
+            [group.group_df for group in groups], ignore_index=False
+        ),
+    )
+    # groups: list[_Group] = []
+
+    # group_key_to_group_df: dict[tuple[Any, ...], pd.DataFrame] = {}
+    # group_key_to_group_size: dict[tuple[Any, ...], int] = {}
+    # for group_name, group_df in groups:
+    #     group_key = tuple(group_name)
+    #     group_key_to_group_df[group_key] = group_df
+    #     group_key_to_group_size[group_key] = len(group_df)
+
+    # if sort_groups_by_cardinality:
+    #     group_keys = sorted(
+    #         group_key_to_group_size.keys(), key=lambda x: group_key_to_group_size[x]
+    #     )
---
+++ crossfilter/core/projection_test.py
@@ -0,0 +1,56 @@
+from io import StringIO
+import pytest
+import pandas as pd
+from syrupy.assertion import SnapshotAssertion
+
+from crossfilter.core.data_subset import DataSubset
+from crossfilter.core.projection import create_grouped_bucketed_projection
+from crossfilter.core.schema import DataSchema as C
+
+
+@pytest.fixture
+def ds() -> DataSubset:
+    csv_data = f"""{C.UUID_STRING},{C.DATA_TYPE},{C.RATING_0_TO_5},{C.NAME},{C.TIMESTAMP_UTC},TIMESTAMP_YEAR,TIMESTAMP_DAY,H3_01,H3_02
+uuid0,GPS,1,name0,2024-07-27T9:00:00,2024-01-01,2024-07-27,abcd,abcde 
+uuid1,GPS,2,name1,2024-07-28T9:00:00,2024-01-01,2024-07-28,abcd,abcde 
+uuid2,GPS,3,name2,2024-07-29T9:00:00,2024-01-01,2024-07-29,abcd,abcde 
+uuid3,GPS,4,name3,2024-07-30T9:00:00,2024-01-01,2024-07-30,abcd,abcde 
+uuid4,PHOTO,5,name4,2024-07-31T9:00:00,2024-01-01,2024-07-31,abcd,abcde 
+uuid5,PHOTO,,name5,2025-08-01T9:00:00,2025-01-01,2025-08-01,abcd,abcde 
+uuid6,,,name6,2025-08-02T9:00:00,2025-01-01,2025-08-02,abcd,abcde 
+uuid7,PHOTO,2,name7,2025-08-03T9:00:00,2025-01-01,2025-08-03,abcd,abcde 
+uuid8,VIDEO,3,name8,2025-08-04T9:00:00,2025-01-01,2025-08-04,abcd,abcde 
+uuid9,,4,name9,,,,abcd,abcde 
+uuid10,VIDEO,5,name10,2025-08-06T9:00:00,2025-01-01,2025-08-06,, 
+"""
+    df = pd.read_csv(StringIO(csv_data))
+    # df = df.set_index(C.UUID_STRING, verify_integrity=True)
+    return DataSubset(subset_uuid="test_subset_uuid", df=df)
+
+
+def test_create_grouped_bucketed_projection_no_grouping_no_bucketing(
+    ds: DataSubset,
+    snapshot: SnapshotAssertion,
+) -> None:
+    projection = create_grouped_bucketed_projection(
+        ds,
+        group_by_columns=(),
+        bucketing_candidate_columns=(),
+        max_entries_per_group=100,
+        sort_groups_by_cardinality=False,
+    )
+    assert projection.projected_data.to_dict(orient="records") == snapshot
+
+
+def test_create_grouped_bucketed_projection_group_by_data_type_no_bucketing(
+    ds: DataSubset,
+    snapshot: SnapshotAssertion,
+) -> None:
+    projection = create_grouped_bucketed_projection(
+        ds,
+        group_by_columns=(C.DATA_TYPE,),
+        bucketing_candidate_columns=(),
+        max_entries_per_group=100,
+        sort_groups_by_cardinality=False,
+    )
+    assert projection.projected_data.to_dict(orient="records") == snapshot
